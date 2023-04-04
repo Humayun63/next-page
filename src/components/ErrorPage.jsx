bfd2b94f2659d8e1e8c062bf7d6c559af19c4918
Humayun63 <humayun126224@gmail.com>
2023-04-04 18:19:02 +0600

    error page added

--- src/components/ErrorPage.jsx
+++ src/components/ErrorPage.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import { Link, useRouteError } from 'react-router-dom';
+import {FaceFrownIcon} from '@heroicons/react/24/solid'
 
 const ErrorPage = () => {
+    const {error, status} = useRouteError();
+    console.log(error, status);
     return (
-        <div>
-            Error Page
+        <div className='h-[100vh] w-full flex items-center justify-center flex-col'>
+            <FaceFrownIcon className='w-36 text-yellow-400'></FaceFrownIcon>
+            <h1 className='text-yellow-500 text-6xl font-bold'>{status || 404}</h1>
+            <h2 className='text-2xl my-4 font-bold text-orange-700'>{error?.message}</h2>
+            <button className='btn bg-blue-500 px-4 py-2 text-white font-medium rounded-md'> <Link to='/'>Go Back To Home</Link> </button>
         </div>
     );
 };
