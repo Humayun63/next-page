91b03dc2d5e026ff697ad9471cb92b58fdd2b1d7
Humayun63 <humayun126224@gmail.com>
2023-04-04 16:03:26 +0600

    loader added to books page

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
+import Loading from './Loading';
+import { useLoaderData, useNavigation } from 'react-router-dom';
 
 const Books = () => {
+    const {books} = useLoaderData();
+    const navigation = useNavigation()
+    console.log(books, navigation.state);
+    if(navigation.state === 'loading') {
+        return <Loading></Loading>
+    }
     return (
         <div>
+            
             Books
         </div>
     );
